fff0ab45f510fdbe7260714d342a186c4231ade8
Owen Sims <owen.sims@cyberlytic.com>
2018-11-13 21:03:14 +0000

    Add utility functions, including the data loader

---
+++ src/backend/utils/data_loader.py
@@ -0,0 +1,19 @@
+import json
+
+from analytics import config
+
+from api.config import BASE_PATH, MONGO_DB_INFO
+from api.services.MongoService import MongoService
+
+
+def insert_default_property_data():
+    json_data = open(BASE_PATH + 'utils/data/belfast_property_data.json')
+    belfast_properties = json.load(json_data)
+    if MongoService().check_collection(
+            collection_name=MONGO_DB_INFO['propertyCollection']):
+        MongoService().drop_database(
+            collection_name=MONGO_DB_INFO['propertyCollection'])
+    MongoService().insert_to_collection(
+        collection_name=MONGO_DB_INFO['propertyCollection'],
+        data=belfast_properties)
+    return belfast_properties
