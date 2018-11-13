3e872943b15f29cd145e37b05050262a768ea8ee
Owen Sims <owen.sims@cyberlytic.com>
2018-11-13 23:18:55 +0000

    Add searchbar and search functionality to homepage

--- src/webserver/gui/home/home.component.ts
+++ src/webserver/gui/home/home.component.ts
@@ -8,4 +8,17 @@ import { Router } from '@angular/router';
 
 export class HomeComponent {
 
+  constructor(
+    private router: Router) {}
+
+  searchQuery:string = "";
+
+  prepareSearchQuery(event) {
+    this.searchQuery = event['target']['value'];
+  };
+
+  propertySearch() {
+    this.router.navigate(['/search'],
+    { queryParams: { q: this.searchQuery } });
+  };
 }
